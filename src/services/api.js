b2de8047006f41f558bcd26f31542b74b3652f81
Chillakuru Vamsi <vamsichillakuru1@gmail.com>
2026-07-15 12:46:29 +0530

    Fixed project structure and backend configuration

--- src/services/api.js
+++
@@ -1,9 +0,0 @@
-import axios from "axios";
-const api=axios.create({
-    // backend url
-    baseURL: "https://localhost:5000/api",
-    headers: {
-        "Content=Type": "application/json",
-    },
-});
-export default api;
